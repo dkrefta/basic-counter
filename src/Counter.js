e2100eda8859fd61d4bfc2aa8ab929d41902a9b1
Diego <diego.krefta@gmail.com>
2019-04-24 18:32:39 -0300

    adjust

--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 
 export default class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  handleIncrement = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  handleDecrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
 
   render() {
+    const { count } = this.state;
     return (
       <section className="Counter">
-        <h1>Count: 0 </h1>
-        <button onClick={() => {}} className="full-width">
+        <h1>Count: {count} </h1>
+        <button onClick={this.handleIncrement} className="full-width">
           Increment
         </button>
-        <button onClick={() => {}} className="full-width">
+        <button onClick={this.handleDecrement} className="full-width">
           Decrement
         </button>
         <button onClick={() => {}} className="full-width">
